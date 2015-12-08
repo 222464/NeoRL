e06592871eae7b6ba713ff2d87bf8b7c1d778592
Eric Laukien <eric@rocketcitystudios.com>
2015-12-08 00:01:07 -0500

    Tuning defaults

--- NeoRL/source/neo/AgentSwarm.h
+++ NeoRL/source/neo/AgentSwarm.h
@@ -44,11 +44,11 @@ namespace neo {
 				_scWeightAlpha(0.002f), _scWeightLambda(0.95f),
 				_scActiveRatio(0.05f), _scBoostAlpha(0.02f),
 				_baseLineDecay(0.01f), _baseLineSensitivity(0.01f),
-				_predWeightAlpha(0.05f),
+				_predWeightAlpha(0.01f),
 				_swarmAnnealingIterations(8), _swarmActionDeriveAlpha(0.05f),
-				_swarmQAlpha(0.002f), _swarmQHiddenAlpha(0.02f),
-				_swarmPredAlpha(0.05f), _swarmLambda(0.95f), _swarmGamma(0.99f),
-				_swarmExpPert(0.05f), _swarmExpBreak(0.02f)
+				_swarmQAlpha(0.01f), _swarmQHiddenAlpha(0.05f),
+				_swarmPredAlpha(0.1f), _swarmLambda(0.95f), _swarmGamma(0.99f),
+				_swarmExpPert(0.04f), _swarmExpBreak(0.01f)
 			{}
 		};
 
