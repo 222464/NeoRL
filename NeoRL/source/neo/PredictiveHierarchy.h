96b9aa626ad0a4e4582e3e9636451bb1f4d722b2
Eric Laukien <eric@rocketcitystudios.com>
2015-12-17 01:57:28 -0500

    Bug fixes

--- NeoRL/source/neo/PredictiveHierarchy.h
+++ NeoRL/source/neo/PredictiveHierarchy.h
@@ -25,9 +25,9 @@ namespace neo {
 			LayerDesc()
 				: _size({ 8, 8 }),
 				_feedForwardRadius(5), _recurrentRadius(5), _lateralRadius(5), _feedBackRadius(6), _predictiveRadius(6),
-				_scWeightAlpha(0.01f), _scWeightRecurrentAlpha(0.005f), _scWeightLambda(0.95f),
+				_scWeightAlpha(0.001f), _scWeightRecurrentAlpha(0.0005f), _scWeightLambda(0.95f),
 				_scActiveRatio(0.05f), _scBoostAlpha(0.1f),
-				_baseLineDecay(0.5f), _baseLineSensitivity(0.01f),
+				_baseLineDecay(0.2f), _baseLineSensitivity(0.01f),
 				_predWeightAlpha(0.05f)
 			{}
 		};
