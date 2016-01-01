861b242d541973aec33bc4fad4091264d7e857e0
Eric Laukien <ericlaukien@gmail.com>
2015-12-31 19:33:21 -0500

    Tuning

--- NeoRL/source/neo/AgentHA.h
+++ NeoRL/source/neo/AgentHA.h
@@ -64,7 +64,7 @@ namespace neo {
 				: _size({ 8, 8 }),
 				_feedForwardRadius(5), _recurrentRadius(5), _lateralRadius(5), _feedBackRadius(6), _predictiveRadius(6),
 				_scWeightAlpha(0.0001f), _scWeightRecurrentAlpha(0.0001f), _scWeightLambda(0.95f),
-				_scActiveRatio(0.1f), _scBoostAlpha(0.002f),
+				_scActiveRatio(0.06f), _scBoostAlpha(0.01f),
 				_predWeightAlpha(0.02f),
 				_qAlpha(0.1f), _qBiasAlpha(0.1f), _qLambda(0.95f), _qRadius(6),
 				_noise(0.05f)
